0c171cb14bea4e72061f1a0014c9eaf77a5a87f2
Oleksandr Kondratov <al.kon.workmail@gmail.com>
2022-03-14 18:09:42 +0200

    removed cwd in helpers/posts-util

--- helpers/posts-util.js
+++ helpers/posts-util.js
@@ -2,7 +2,7 @@ import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
 
-const postsDirectory = path.join(process.cwd(), "posts");
+const postsDirectory = path.join("posts");
 
 function getPostData(postIdentifier) {
   const postSlug = postIdentifier.replace(/\.md$/, ""); // removes the file extension
